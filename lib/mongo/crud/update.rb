1e76af509caffbf0f8f8d24a440a5e60eaafe4c0
Frank Hall <ChapterHouse.Dune@gmail.com>
2014-02-08 22:29:25 -0600

    Added missing result to Crud::Update

--- lib/mongo/crud/update.rb
+++ lib/mongo/crud/update.rb
@@ -8,7 +8,7 @@ class Mongo::Crud::Update < Mongo::Crud
     kwery = query.dup
     kwery['_id'] = BSON::ObjectId.from_string(kwery.delete('id').to_i.to_s(16)) if kwery.has_key?('id')
     collection.update(kwery, {"$set" =>  bound_data }, options)
-    Mongo::Crud::Read.new(collection, kwery)
+    Mongo::Crud::Read.new(collection, kwery).results
   end
 
 end
